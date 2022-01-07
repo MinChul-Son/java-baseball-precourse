be3077632d9e8a522859b125b2ba453e4dcdfc5e
MinChul-Son <smc5236@naver.com>
2022-01-07 15:15:16 +0900

    🔥 fix : 난수 타입 변경

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -4,16 +4,16 @@ import nextstep.utils.Randoms;
 
 public class Computer {
 
-    protected final int number;
+    protected final String number;
 
     public Computer(final int start, final int end) {
         this.number = generateRandomNumber(start, end);
     }
 
-    private int generateRandomNumber(final int start, final int end) {
-        return Integer.valueOf(String.valueOf(Randoms.pickNumberInRange(start, end))
+    private String generateRandomNumber(final int start, final int end) {
+        return String.valueOf(Randoms.pickNumberInRange(start, end))
                 + String.valueOf(Randoms.pickNumberInRange(start, end))
-                + String.valueOf(Randoms.pickNumberInRange(start, end)));
+                + String.valueOf(Randoms.pickNumberInRange(start, end));
     }
 
 }
