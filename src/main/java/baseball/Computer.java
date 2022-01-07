125787b6c821daa88f12950f92ce1bd65cedee8e
MinChul-Son <smc5236@naver.com>
2022-01-07 20:55:55 +0900

    🔥 fix : 누락된 기능 요구 사항 추가
    - 각 수가 유니크한 난수로 생성되어야한다는 조건을 누락해 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,18 +2,38 @@ package baseball;
 
 import nextstep.utils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
 
-    protected final String number;
+    protected String number;
 
     public Computer(final int start, final int end) {
         this.number = generateRandomNumber(start, end);
     }
 
     private String generateRandomNumber(final int start, final int end) {
-        return String.valueOf(Randoms.pickNumberInRange(start, end))
-                + String.valueOf(Randoms.pickNumberInRange(start, end))
-                + String.valueOf(Randoms.pickNumberInRange(start, end));
+
+        List<Integer> uniqueNumberList = new ArrayList<>();
+        int randomNumber;
+        StringBuilder stringBuilder = new StringBuilder();
+
+
+        while (uniqueNumberList.size() < 3) {
+            randomNumber = Randoms.pickNumberInRange(start, end);
+            if (uniqueNumberList.contains(randomNumber)) {
+                continue;
+            }
+
+            uniqueNumberList.add(randomNumber);
+        }
+
+        for (Integer number : uniqueNumberList) {
+            stringBuilder.append(number);
+        }
+
+        return stringBuilder.toString();
     }
 
 }
