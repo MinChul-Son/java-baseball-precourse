9a182a04423e63988fc5bc5ca0c92abd9d0538da
MinChul-Son <smc5236@naver.com>
2022-01-07 15:16:08 +0900

    🔥 feat : 볼 & 스트라이커 판별기 구현

---
+++ src/main/java/baseball/BaseballChecker.java
@@ -0,0 +1,38 @@
+package baseball;
+
+public class BaseballChecker {
+
+    private int ball;
+    private int strike;
+
+    private final static int ZERO = 0;
+    private final static int LAST_INDEX = 3;
+
+    public BaseballChecker() {
+        this.ball = ZERO;
+        this.strike = ZERO;
+    }
+
+
+    public boolean ballCounter(final String computer, final String player) {
+        // 정답! - 얼리 리턴
+        if (computer.equals(player)) {
+            return true;
+        }
+
+        // 볼 & 스트라이크
+        for (int extractIndex = ZERO; extractIndex < LAST_INDEX; extractIndex++) {
+            if (computer.substring(extractIndex, extractIndex + 1).equals(player.substring(extractIndex, extractIndex + 1))) {
+                this.strike++;
+                continue;
+            }
+
+            if (computer.indexOf(player.substring(extractIndex, extractIndex + 1)) >= 0) {
+                ball++;
+            }
+
+        }
+
+        return false;
+    }
+}
